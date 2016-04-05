18ada132fea3b74b207ba8dd3081a0072990b641
Victoria Hayes <victoria.hayes@mymail.champlain.edu>
2016-04-05 14:47:57 -0400

    made a class for client input; working on passing it as a json string

---
+++ musicapiclient/src/musicapiclient/jsonConverter.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package musicapiclient;
+
+import org.json.*;
+/**
+ *
+ * @author Victoria
+ */
+public class jsonConverter {
+    public jsonConverter()
+    {}
+    
+    public String toJsonString(QueryParams)
+    {
+        try
+        {
+            JSONObject jsonObj=new JSONObject();
+        }
+        catch(Exception ex)
+        {
+            System.out.println("Error");
+        }
+        return (" ");
+    }
+}
