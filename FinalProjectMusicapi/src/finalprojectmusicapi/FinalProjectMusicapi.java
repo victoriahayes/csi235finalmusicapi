5d15f9edabd9c0724ca6459659268ca3bbd280aa
gendium <alec.rulev@mymail.champlain.edu>
2016-04-05 14:51:49 -0400

    fetch
    Work on the fetch function - TODO: change parameter for fetch function
    and get info from client

--- FinalProjectMusicapi/src/finalprojectmusicapi/FinalProjectMusicapi.java
+++ FinalProjectMusicapi/src/finalprojectmusicapi/FinalProjectMusicapi.java
@@ -20,6 +20,8 @@ final String SITEACCESS = "http://developer.echonest.com/api/v4/song/search?api_
     /**
      * @param args the command line arguments
      */
+
+//Convert function into song object
 public static Movie[] parseData(String songJsonStr, int numSongs)
 {
 	Movie[] songs = new Movie[numSongs];
@@ -35,6 +37,56 @@ public static Movie[] parseData(String songJsonStr, int numSongs)
 	}catch(JSONException e){System.out.println(e.getMessage());}
 	return songs;
 }
+
+// issue: figure out how to fetch what you're searching for from client
+public static String fetchData(int year)
+{
+	HttpURLConnection urlConnection = null;
+	BufferedReader reader = null;
+	String jsonString = null;
+	try
+	{
+		String sUrl= SITEACCESS;// + year + "&sort_by=vote_average.desc&api_key=" + APIKEY;
+		URL url = new URL(sUrl);
+		urlConnection = (HttpURLConnection) url.openConnection();   
+		urlConnection.setRequestMethod("GET"); 
+		urlConnection.connect();
+	  
+	  InputStream inputStream = urlConnection.getInputStream();
+	  
+	  reader = new BufferedReader(new InputStreamReader(inputStream));
+	  String line;    
+	  StringBuilder buffer = new StringBuilder();       
+	  while ((line = reader.readLine()) != null) 
+	  { 
+		 buffer.append(line).append("\n");  
+	  }
+	  jsonString = buffer.toString();
+	}catch(IOException e)
+	{
+		System.out.println(e.getMessage());
+	}
+	finally
+	{
+		if (urlConnection != null)
+		{ 
+		 urlConnection.disconnect();     //disconnect  
+		}  
+		if (reader != null)
+		{                
+			try           
+			 {            
+				 reader.close();    //close the input stream  
+			 }                
+			catch (final IOException e){
+				 System.out.println(e.getMessage()); }
+		}
+		else
+			return jsonString;
+		 }
+	return jsonString;
+	}
+	
     public static void main(String[] args) {
         // TODO build this to be the server side
         /* Goals:
